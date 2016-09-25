b11fee9be8fccfea8da9977838506dcebeb87f96
Who <Who@F.local>
2016-09-25 21:34:02 +0800

    initial commit

--- vim/spf13-vim-3.0/bootstrap.sh
+++ vim/spf13-vim-3.0/bootstrap.sh
@@ -17,7 +17,7 @@
 ############################  SETUP PARAMETERS
 app_name='spf13-vim'
 [ -z "$APP_PATH" ] && APP_PATH="$HOME/dotfiles/vim/.spf13-vim-3"
-[ -z "$REPO_URI" ] && REPO_URI='https://F____@bitbucket.org/F____/dotfiles.git'
+#[ -z "$REPO_URI" ] && REPO_URI='https://F____@bitbucket.org/F____/dotfiles.git'
 [ -z "$REPO_BRANCH" ] && REPO_BRANCH='3.0'
 debug_mode='0'
 fork_maintainer='0'
