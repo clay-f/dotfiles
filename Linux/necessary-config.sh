87a449c178d89e7419a82142b4e1534bab359bf7
Who <Who@F.local>
2016-09-26 06:38:07 +0800

    add ack-grep for linux config file

--- Linux/necessary-config.sh
+++ Linux/necessary-config.sh
@@ -5,17 +5,17 @@ position=${HOME}/dotfiles
 apt_install()
 {
     sudo apt-get -y install gcc
+    sudo apt-get -y install make
     sudo apt-get -y install sudo
     sudo apt-get -y install tmux
     sudo apt-get -y install vim
     sudo apt-get -y install nmap
     sudo apt-get -y install tree
     sudo apt-get -y install pstree
-    sudo apt-get -y install gcc
-    sudo apt-get -y install make
     sudo apt-get -y install zsh
     sudo apt-get -y install curl
     sudo apt-get -y install lua5.3
+    sudo apyt-get -y install ack-grep
     #wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - |sh
     sudo apt-get autoclean
 }
