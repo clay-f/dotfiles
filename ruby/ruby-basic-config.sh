aa9a7c68cdc300da5a31228e0dd4e3d6f7d10141
Who <Who@F.local>
2016-09-27 14:56:29 +0800

    non

--- ruby/ruby-basic-config.sh
+++ ruby/ruby-basic-config.sh
@@ -2,7 +2,7 @@
 #created by F on 2016-09-19
 rvm_ruby()
 {
-    curl -sSL https://rvm.io/mpapis.asc | gpg2 --import - 
+    curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
     curl -sSL https://get.rvm.io | bash -s stable
     rvm install 2.3.1
     gem sources --add https://ruby.taobao.org/ --remove https://rubygems.org/
