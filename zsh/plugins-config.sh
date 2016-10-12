3b47aa4fb86a9828365356dac2d0822ad793bca2
Who <Who@F.local>
2016-10-12 16:06:40 +0800

    initial commit

--- zsh/plugins-config.sh
+++ zsh/plugins-config.sh
@@ -2,4 +2,7 @@
 #created by F on 2016-09-19
 
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+if [ -e $HOME/.oh-my-zsh/custom/example.zsh ]; then
+    echo "bindkey '^ ' autosuggest-accept" >> $HOME/.oh-my-zsh/custom/example.zsh
+fi
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
