b73a592b0b6cec277f964d18f0fbf3b71c21c041
Who <Who@F.local>
2016-10-16 10:56:26 +0800

    improve update

--- tools/uninstall.sh
+++ tools/uninstall.sh
@@ -1,30 +1,33 @@
 #!/usr/bin/env sh
 
-
 uninstall_vim() {
-    app_dir="$HOME/dotfiles/vim"
-
-    warn() {
-        echo "$1" >&2
-    }
-
-    die() {
-        warn "$1"
-        exit 1
-    }
 
     rm $HOME/.vimrc
     rm $HOME/.vimrc.bundles
-    rm $HOME/.vim
-
-    rm -rf $app_dir
+    rm -rf $HOME/.vim
+    rm -rf $HOME/.vimbackup/
+    rm -rf $HOME/.vimswap/
+    rm -rf $HOME/.vimundo/
+    rm -rf $HOME/.vimviews/
 }
 
 uninstall_dotfiles() {
     dotfile_dir="$HOME/dotfiles"
     rm -rf $HOME/dotfiles
-    echo "\t\tUninstall Success"
 }
 
+uninstall_zsh()
+{
+    bash ~/.oh-my-zsh/tools/uninstall.sh
+}
+
+success_uninstall()
+{
+    echo "\t\tUninstall Success"
+}
 uninstall_vim
 uninstall_dotfiles
+uninstall_zsh
+if [ ! -d $HOME/dotfiles ]; then
+    success_uninstall
+fi
