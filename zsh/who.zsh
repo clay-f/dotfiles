0d82c23420ecd177f574b13e231b92ba0c32fe96
F <liyongfeit@gmail.com>
2016-12-06 14:07:49 +0800

    add alias git config

--- zsh/who.zsh
+++ zsh/who.zsh
@@ -4,13 +4,13 @@ alias ovi='open -a MacVim '
 
 alias gref="git reflog --pretty=format:\"%h : %an > %ar : %s\""
 
-#environment setting {
+# environment setting {
     PATH="/usr/local/bin:/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
     export PATH="/usr/local/sbin:$PATH"
     export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
-#}
+# }
 
-#sources configuration{
+# sources configuration{
     source $HOME/dotfiles/zsh/.aliases
     [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
-#}
+# }
