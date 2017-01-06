252f33a196ab5ba36461f8d222b5954bef8ece61
F <liyongfeit@gmail.com>
2017-01-06 10:10:38 +0800

    add debug for linux_config

--- etc/linux-config.sh
+++ etc/linux-config.sh
@@ -2,6 +2,36 @@
 
 position=${HOME}/dotfiles
 
+
+msg() {
+    printf "%b\n" "$1" >&2
+}
+
+error() {
+    msg "\33[31m[✘]\33[0m ${1}${2}"
+    exit 1
+}
+
+program_exists() {
+    local ret='0'
+    command -v $1 >/dev/null 2>&1 || { local $ret='1'; }
+
+    if [[ "$ret" -ne 0 ]]; then
+        return 1
+    fi
+
+    return 0
+}
+
+program_must_exist() {
+    program_exist $1
+
+    if [[ "$?" -ne 0 ]]; then
+        error "You muse have '$1' installed  to continue."
+        exit
+    fi
+}
+
 toolkits_config() {
     sudo apt-get -y install gcc
     sudo apt-get -y install make
@@ -46,6 +76,8 @@ nesessary() {
 }
 
 install() {
+    progrm_exists "git"
+
     if [ -d $position ];then
         nesessary
         develop_config
