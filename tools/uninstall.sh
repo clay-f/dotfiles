5f4c31a3212b9cdbd262ee248037642326eeb6d9
F <liyongfeit@gmail.com>
2017-01-19 10:47:20 +0800

    remove redundancy code

--- tools/uninstall.sh
+++ tools/uninstall.sh
@@ -1,47 +1,11 @@
-#!/usr/bin/env bash
-
-uninstall_dotfiles() {
+uninstall() {
     dotfile_dir="$HOME/dotfiles"
     rm -rf $HOME/dotfiles
-}
+    rm -rf $HOME/.vim*
 
-uninstall_vim() {
-    rm $HOME/.vimrc
-    rm $HOME/.vimrc.bundles
-    rm -rf $HOME/.vim
-    rm -rf $HOME/.vimbackup/
-    rm -rf $HOME/.vimswap/
-    rm -rf $HOME/.vimundo/
-    rm -rf $HOME/.vimviews/
-}
-
-uninstall_zsh() {
-    bash ~/.oh-my-zsh/tools/uninstall.sh
-}
-
-uninstall_ruby() {
-    rm -rf ~/.rvm
-    rm -rf ~/.gem
-    rm prf ~/.gemrc
-}
-
-uninstall_tmux() {
-    rm -rf ~/.tmux.conf
-}
-
-success_uninstall() {
-    echo "\t\tUninstall Success"
-}
-
-main() {
-    uninstall_vim
-    uninstall_dotfiles
-    uninstall_zsh
-    uninstall_tmux
-    uninstall_ruby
     if [ ! -d $HOME/dotfiles ]; then
-        success_uninstall
+        echo -e "\t\tUninstall Successed"
     fi
 }
 
-main
+uninstall
