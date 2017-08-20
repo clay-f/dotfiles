2aea5becce0e033270fe22032716aa192abdefc8
F <839322915@qq.com>
2017-08-20 18:36:29 +0800

    Update format

--- etc/brew.sh
+++ etc/brew.sh
@@ -3,7 +3,7 @@
 set -e
 
 brew install wget --with-iri
-brew install the_silver_searcher aria2 ctags zsh rbenv ruby-build nmap autojump  gawk imagemagick
+brew install git the_silver_searcher aria2 ctags zsh rbenv ruby-build nmap autojump  gawk imagemagick
 brew install vim --override-system-vi 
 brew cleanup
 
