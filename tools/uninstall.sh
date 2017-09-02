658985b162e177fbe4fed66a2bce40011bb26e20
F <839322915@qq.com>
2017-09-02 10:18:10 +0800

    update output format

--- tools/uninstall.sh
+++ tools/uninstall.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
 confirm() {
-    printf "Type keyword [y/n]: "
+    printf "\rType keyword [y/n]: "
     sleep 1
 }
 
 trap 'confirm' SIGINT
 
-input='$@'
-
 while [[ 1 ]]; do
     printf "Type keyword [y/n]: "
-    read -n 1 input
+    read  -s -n 1 input
     if [[ "$input" =~ [Yy] ]]; then
         rm -rf ~/dotfiles && exit 0
     elif [[ "$input" =~ [Nn] ]]; then
             exit 0
     fi
-    echo $REPLY
+    printf "\n"
     bash -c "$input"
 done
