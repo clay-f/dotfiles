49e574e591c45a89ee76f2b0c45656a9db214e54
f <839322915@qq.com>
2017-10-09 16:02:54 +0800

    update

--- etc/brew.sh
+++ etc/brew.sh
@@ -2,9 +2,8 @@
 #
 # install help terminal tools
 
-brew install git the_silver_searcher aria2 zsh autojump tree
+brew install git the_silver_searcher aria2 zsh autojump tree gawk
 brew install rbenv ruby-build nmap
-brew install gawk coreutils
 brew install wget --with-iri
 brew install gnu-sed --with-default-names
 brew install vim --override-system-vi
