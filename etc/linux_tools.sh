680635795d86f438c90ff40c0594d873ac785d06
f <839322915@qq.com>
2017-11-29 11:51:00 +0800

    Fixed permission error when add deb source

--- etc/linux_tools.sh
+++ etc/linux_tools.sh
@@ -1,16 +1,32 @@
+#!/usr/bin/env bash
 # linux utility tools
 
-wget -P /tmp https://repo.mysql.com//mysql-apt-config_0.8.9-1_all.deb -D
-dpkg -i mysql-apt-config_0.8.9-1_all.deb
+declare -r today="date_formate='date +"%Y_%m_%d_%H_%M"'"
+
+printf "start config deb sources config...\n"
+if [[ -f "${HOME}/.dotfiles/etc/sources.list" ]]; then
+    mv /etc/apt/sources.list "/etc/apt/sources_list_${today}.backup"
+    cp "${HOME}/.dotfiles/etc/sources.list" /etc/apt/sources.list
+fi
+
+wget -P /tmp https://repo.mysql.com//mysql-apt-config_0.8.9-1_all.deb
+sudo dpkg -i /tmp/mysql-apt-config_0.8.9-1_all.deb
 sudo apt-get update
 
-tools=(git aria2 emacs mysql-server make gcc nginx )
+echo "Start install common tools..."
+tools=(git aria2 emacs mysql-server make gcc nginx net-tools)
+sudo apt-get -y install "${tools[@]}"
 
-for i in ${tools[@]}
-do
-    sudo apt-get -y install $i
-done
-
+# config ruby env
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 cd ~/.rbenv && src/configure && make -C src
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+
+if [[ -f "${HOME}/.aliases.sh" ]]; then
+    echo 'source ~/.aliases.sh"' >> ~/.bashrc
+fi
+
+sudo apt-get upgrade
+sudo apt-get autoremove
+printf "tools config have done\n"
