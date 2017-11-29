ccb8d3637964b0c927a1750db3baac8788df2325
f <839322915@qq.com>
2017-11-29 17:52:45 +0800

    update

--- etc/linux_tools.sh
+++ etc/linux_tools.sh
@@ -3,12 +3,6 @@
 
 declare -r today="date_formate='date +"%Y_%m_%d_%H_%M"'"
 
-printf "start config deb sources config...\n"
-if [[ -f "${HOME}/.dotfiles/etc/sources.list" ]]; then
-    mv /etc/apt/sources.list "/etc/apt/sources_list_${today}.backup"
-    cp "${HOME}/.dotfiles/etc/sources.list" /etc/apt/sources.list
-fi
-
 wget -P /tmp https://repo.mysql.com//mysql-apt-config_0.8.9-1_all.deb
 sudo dpkg -i /tmp/mysql-apt-config_0.8.9-1_all.deb
 sudo apt-get update
@@ -27,6 +21,6 @@ if [[ -f "${HOME}/.aliases.sh" ]]; then
     echo 'source ~/.aliases.sh"' >> ~/.bashrc
 fi
 
-sudo apt-get upgrade
-sudo apt-get autoremove
+sudo apt-get -y upgrade
+sudo apt-get -y autoremove
 printf "tools config have done\n"
