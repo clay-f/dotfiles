cc8ef162d86e748dc260e0ac29f62bafcbcfbe44
f <839322915@qq.com>
2018-03-10 10:43:30 +0800

    update aliases, delete zshrc file

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -11,7 +11,6 @@ alias h="history | tail -n 50"
 alias df='df -h'
 alias du_sum_directory='du -hs'
 
-# find information in files
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
@@ -20,9 +19,4 @@ alias cleanup_ds_store_files="find . -type f -name '*.DS_Store' -ls -delete"
 
 alias k9="kill -9"
 
-### listen port, show prossess info
-alias top_load_mem="top -l 1 -s 0 | awk '/PhysMem/ || /Load Avg/ {print}'"
-alias top_order_cpu="top -o cpu -n 20"
 alias lsof_listen="lsof -i TCP -s TCP:LISTEN -n -P"
-
-alias date_formate="date +\"%Y_%m_%d_%H_%M\""
