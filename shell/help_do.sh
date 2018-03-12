b575b29ea4861935b167257400f76da2a6172bf2
f <839322915@qq.com>
2018-03-12 19:43:26 +0800

    update awk reg

--- shell/help_do.sh
+++ shell/help_do.sh
@@ -12,5 +12,5 @@ load_files "${HOME}/.aliases.sh" "${HOME}/.develop_env.sh"
 
 kill_process_by_name() {
     declare locale process_name=$1
-    kill -9 $(ps aux | ag ${process_name} | ag -v "ag ${process_name}" | awk '{print $2}')
+    kill -9 $(ps aux | ag ${process_name} | awk '!/ag/ {print $2}')
 }
