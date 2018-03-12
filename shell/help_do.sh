658c1d088f27ea5003d72e5c6c45f5281427268a
f <839322915@qq.com>
2018-03-12 17:28:21 +0800

    add kill process function

--- shell/help_do.sh
+++ shell/help_do.sh
@@ -8,4 +8,9 @@ load_files() {
 	unset file
 }
 
-load_files "${HOME}/.aliases.sh"
+load_files "${HOME}/.aliases.sh" "${HOME}/.develop_env.sh"
+
+kill_process_by_name() {
+    declare locale process_name=$1
+    kill -9 $(ps aux | ag ${process_name} | ag -v "ag ${process_name}" | awk '{print $2}')
+}
